90e86a5c610a1d006473174161587d370c1c2215
sudheerj <jsudheer50@gmail.com>
2018-03-11 23:32:47 +0800

    Add SPA for GitHub Pages boilerplate

---
+++ README.md
@@ -0,0 +1,5 @@
+## React Github Viewer using React, Axios and PrimeReact
+
+### How to build and run
+``npm install``
+``npm start``
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import './index.css';
+import Search from './views/Search';
+import Github from './views/Github';
+
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/omega/theme.css';
+import 'font-awesome/css/font-awesome.css';
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path="/" component={Search}/>
+            <Route path="/:username" component={Github}/>
+        </div>
+    </Router>, document.getElementById('root'));
+
+
