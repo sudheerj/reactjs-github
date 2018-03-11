dbc9b8fce27b8d392bdb3d7b38d449bfd44bee3e
sudheerj <jsudheer50@gmail.com>
2018-03-11 23:24:52 +0800

    Create github pages for single page apps

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: `${__dirname}/src/index.js`,
+    output: {
+        path: `${__dirname}/build`,
+        publicPath: '/build/',
+        filename: 'bundle.js',
+    },
+
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.css$/, loader:['style-loader', 'css-loader']},
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file-loader?name=assets/[name].[hash].[ext]'
+            }
+        ],
+    },
+
+    plugins: process.argv.indexOf('-p') === -1 ? [] : [
+        new webpack.optimize.UglifyJsPlugin({
+            output: {
+                comments: false,
+            },
+        }),
+    ],
+};
